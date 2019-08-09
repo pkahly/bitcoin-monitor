81abcaa0b1335538b9466c6e6e0e1514d2d57e97
philip <pkahly@gmail.com>
2019-08-08 20:18:18 -0400

    #92 Remove Interactive Menu and replace with flags

--- main.py
+++ main.py
@@ -4,94 +4,43 @@ import argparse
 from lib import bitcoin_alerter, install, debug
 
 
-def install_menu():
-   while True:
-      print()
-      print("1. Install Alerter")
-      print("2. Import Historical Price Data")
-      print("3. Store Block Info")
-      print("4. Uninstall Alerter")
-      print("5. Back")
-      
-      choice = int(input("> "))
-
-      if choice == 1:
-         install.install()
-      elif choice == 2:
-         install.import_historical_prices()
-      elif choice == 3:
-         install.add_blocks()
-      elif choice == 4:
-         install.uninstall()
-      else:
-         return
-
-
-def debug_menu():
-   while True:
-      print()
-      print("1. Print Blocks")
-      print("2. Print Price History")
-      print("3. Print Status History")
-      print("4. Back")
-      
-      choice = int(input("> "))
-
-      if choice == 1:
-         debug.print_blocks()
-      elif choice == 2:
-         debug.print_price_history()    
-      elif choice == 3:
-         debug.print_status_history()
-      else:
-         return
-         
-         
-def main_menu():
-   while True:
-      print()
-      print("Welcome to the Bitcoin-Alerter")
-      print("1. Run the Bitcoin Alerter")
-      print("2. Installation Options")
-      print("3. Configuration")
-      print("4. Debugging")
-      print("5. Administration")
-      print("6. Exit")
-
-      choice = int(input("> "))
-
-      if choice == 1:
-         print("Running Alerter...")
-         bitcoin_alerter.run_bitcoin_alerter()
-      elif choice == 2:
-         install_menu()
-      elif choice == 3:
-         print("Not implemented")
-      elif choice == 4:
-         debug_menu()
-      elif choice == 5:
-         print("Not implemented")
-      else:
-         exit()
-
-
-##############################################################
-
-
 parser = argparse.ArgumentParser(description='Tools for sending status and alerts for a full Bitcoin node')
+
 parser.add_argument('--run',
                        action='store_true',
-                       help='run the alerter (skip menu)')
+                       help='run the alerter')
 parser.add_argument('--install',
                        action='store_true',
-                       help='run install scripts (skip menu)')
+                       help='create the database and populate with historical prices and blocks')
+parser.add_argument('--uninstall',
+                       action='store_true',
+                       help='dump the database tables')                       
+parser.add_argument('--print_blocks',
+                       action='store_true',
+                       help='Print the contents of the blocks database')
+parser.add_argument('--print_price_history',
+                       action='store_true',
+                       help='Print the contents of the price database')
+parser.add_argument('--print_status_history',
+                       action='store_true',
+                       help='Print the contents of the status database')                                                                     
+
 args = parser.parse_args()
+
 
 if args.install:
    install.install()
    install.import_historical_prices()
    install.add_blocks()
+elif args.uninstall:
+   install.uninstall()
 elif args.run:
    bitcoin_alerter.run_bitcoin_alerter()
+elif args.print_blocks:
+   debug.print_blocks()
+elif args.print_price_history:
+   debug.print_price_history()
+elif args.print_status_history:
+   debug.print_status_history()
 else:
-   main_menu()
+   print("Use '--help' to see options")
