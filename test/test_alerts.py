756d367fe0869cab5b6f05b2d6c054ed70034feb
philip <pkahly@gmail.com>
2019-08-08 21:21:28 -0400

    #29 Initial Unittest Format

---
+++ test/test_alerts.py
@@ -0,0 +1,15 @@
+import unittest
+from lib import alerts
+
+class TestAlerts(unittest.TestCase):
+   def test_blocks(self):
+      print()
+      
+   def test_difficulty(self):
+      print()
+      
+   def test_reorg(self):
+      print()
+
+   def test_price(self):
+      print()
