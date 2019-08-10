6d974afd0b664659f7e1abd2fd5f049d242376e1
philip <pkahly@gmail.com>
2019-08-10 13:47:38 -0400

    Add experimental script to compute average transaction age

--- experimental/transaction_age.py
+++ experimental/transaction_age.py
@@ -8,9 +8,9 @@ from lib import bitcoin_node_api
 client = bitcoin_node_api.BitcoinAPIClient()
 
 max_height = client.get_num_blocks()
-height = max_height#1
+height = 1
 
-while height < max_height:
+while height <= max_height:
    current_block = client.get_block(height)
    current_block_time = current_block["mediantime"]
    tx_list = current_block["tx"]
@@ -41,5 +41,4 @@ while height < max_height:
       average_age = 0
    
    print("Average TX Age: {:.0f}".format(average_age))
-   time.sleep(1)
-   height -= 1#+= 1
+   height += 1
