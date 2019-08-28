7f39b035ed49afea76cd6a3175379b2fc1c419c3
philip <pkahly@gmail.com>
2019-08-28 18:47:49 -0400

    #145 Add grouping for network hashrate chart

--- charts/hashrate_chart.py
+++ charts/hashrate_chart.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 
 import sqlite3
+from statistics import mean
 import plotly.graph_objects as go
 
+DAY = 144
+WEEK = 1008
+MONTH = 4032
+YEAR = 52560
+
+USE_LOG_SCALE = True
+GROUPING = YEAR
+
+def combine_group(group):
+   # Use first height
+   height = group[0][0]
+   
+   # Use mean of hashrates   
+   hashrates = [result[1] for result in group]
+   hashrate = int(mean(hashrates));
+   
+   return (height, hashrate)
 
 if __name__ == "__main__":
    # Chart fields
@@ -12,22 +30,35 @@ if __name__ == "__main__":
    # Collect Historical Hashrates
    connection = sqlite3.connect("bitcoin.db")
    cursor = connection.cursor()
-   cursor.execute("SELECT height, networkhashps FROM block_info ORDER BY height ASC")
+   
+   # Exclude genesis block as its networkhashps is very high
+   cursor.execute("SELECT height, networkhashps FROM block_info WHERE height > 0 ORDER BY height ASC")
    
    result = cursor.fetchone()
+   group = []
+   
    while result != None:
-      height = result[0]
-      hashrate = result[1]
-      
-      blocks.append(height)
-      hashrates.append(hashrate)
-      
+      # Add to group
+      if len(group) < GROUPING:
+         group.append(result)
+         
+      # Else combine group into a mean
+      else:
+         height, hashrate = combine_group(group)
+         group = []
+         
+         blocks.append(height)
+         hashrates.append(hashrate)
+         
       result = cursor.fetchone()
 
    connection.close()
 
    # Build chart
    fig = go.Figure(data=[go.Scatter(x=blocks, y=hashrates)], )
+   
+   if USE_LOG_SCALE:
+      fig.update_yaxes(type="log")
                          
    fig.update_layout(xaxis_rangeslider_visible=False)
    fig.show(renderer="browser")
